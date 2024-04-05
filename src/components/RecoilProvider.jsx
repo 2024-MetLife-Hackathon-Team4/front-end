439206237ab9643255f344a48385eff936ca8855
hanseulhee <3021062@gmail.com>
2024-04-05 13:39:22 +0900

    apply recoil provider

---
+++ src/components/RecoilProvider.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+
+function RecoilProvider({ children }) {
+  return <RecoilRoot>{children}</RecoilRoot>;
+}
+
+export default RecoilProvider;
